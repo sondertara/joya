f76c4d373c93a40a867c59d865001572fbb58109
SonderTara <xhhuangchn@outlook.com>
2021-12-15 13:04:42 +0800

    fix : alias parse error

--- src/main/java/com/sondertara/joya/core/jdbc/BeanRowMapper.java
+++ src/main/java/com/sondertara/joya/core/jdbc/BeanRowMapper.java
@@ -2,7 +2,6 @@ package com.sondertara.joya.core.jdbc;
 
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
-import java.lang.String;
 
 /**
  * 将一行数据转换成JavaBean
@@ -22,7 +21,7 @@ public class BeanRowMapper<T> implements RowMapper<T> {
             int count = row.getColumnCount();
             T bean = type.getDeclaredConstructor().newInstance();
             for (int i = 1; i <= count; i++) {
-                PropertyDescriptor pd = new PropertyDescriptor((String)row.getColumnLabel(i), type);
+                PropertyDescriptor pd = new PropertyDescriptor((String) row.getColumnLabel(i), type);
                 Method setter = pd.getWriteMethod();
                 setter.invoke(bean, row.getObject(i));
             }
